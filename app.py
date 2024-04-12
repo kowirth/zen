509230867ea3c64d58cfba4058e5c9e486b7ae34
kowirth <166262726+kowirth@users.noreply.github.com>
2024-04-11 20:49:56 -0400

    org commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# Instructions For Tuesday
+
+# python-in-docker and then a few docker containers
+# get arango db (graph db) stood up, and get langchain so it can call the db
+# get langchain so it can call the db. the db be mostly empty but that's fine
+# work on some way to get a word document parsed and into the arango db
+
+# Prereqs
+
+# ensure you have task, docker, git, pipenv, and python3 installed locally
+# and the docker daemon is running
+# Task is https://taskfile.dev/
+# packages: pypandoc = "*",, nltk = "*", [dev-packages]: bumpversion = "*", coverage = "*"
+# pre-commit = "*", pytest = "*", pytest-cov = "*", pytest-mock = "*", refurb = "*"
+
